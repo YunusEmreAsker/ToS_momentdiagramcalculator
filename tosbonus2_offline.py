96fd7e6244b7ded796648b537cb6dec5503ce547
Yunus Emre Asker <yunusemreaskerr@gmail.com>
2023-12-27 23:08:11 +0300

    ToS Bonus Assignment 2 - Soluations

--- tosbonus2_offline.py
+++ tosbonus2_offline.py
@@ -1,21 +1,25 @@
-
 from beambending import Beam
 import matplotlib.pyplot as plt
 
-def calculator():
-    beam = Beam(10)  # Initialize a Beam object of length 10m
 
-    beam.pinned_support = 0    # x-coordinate of the pinned support
-    beam.rolling_support = 10  # x-coordinate of the rolling support
+# Creating a function to calculation
+def calculator():
+    beam = Beam(10)  # # Beam's length
+
+    beam.pinned_support = 0  # Pinned Support Location
+    beam.rolling_support = 10  # Rolling Support Location
 
     from beambending import DistributedLoadV, PointLoadV
 
-    beam.add_loads((
-                PointLoadV(-80, 2),  # 80kN downwards, at x=3m
-                PointLoadV(-100, 9),  # 100kN downwards, at x=9m
-                DistributedLoadV(-16, (0, 7)),  # 16kN/m, downward, for 0m <= x <= 7m
-                DistributedLoadV(-8, (4, 10)),  # -8kN/m,  downwards, for 4m <= x <= 10m
-            ))
+    beam.add_loads([
+        PointLoadV(-80, 2),  # 80kN downwards, at x=3m
+        PointLoadV(-100, 9),  # 100kN downwards, at x=9m
+        DistributedLoadV(-16, (0, 7)),  # 16kN/m, downward, for 0m <= x <= 7m
+        DistributedLoadV(-8, (4, 10)),  # -8kN/m,  downwards, for 4m <= x <= 10m
+    ])
+
     beam.plot()
     plt.show()
+
+
 calculator()
--- tosbonus2_streamlit.py
+++ tosbonus2_streamlit.py
@@ -4,26 +4,26 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 
 
+# Creating a function to calculation
 def calculator():
-    beam = Beam(10)
-    beam.pinned_support = 0
-    beam.rolling_support = 10
+    beam = Beam(10)  # Beam's length
+    beam.pinned_support = 0  # Pinned Support Location
+    beam.rolling_support = 10  # Rolling Support Location
 
     from beambending import DistributedLoadV, PointLoadV
 
+    # Define loads and locations. [load,location]
     beam.add_loads([
-        PointLoadV(-80, 2),
-        PointLoadV(-100, 9),
-        DistributedLoadV(-16, (0, 7)),
-        DistributedLoadV(-8, (4, 10)),
+        PointLoadV(-80, 2),  # 80kN downwards, at x=3m
+        PointLoadV(-100, 9),  # 100kN downwards, at x=9m
+        DistributedLoadV(-16, (0, 7)),  # 16kN/m, downward, for 0m <= x <= 7m
+        DistributedLoadV(-8, (4, 10)),  # -8kN/m,  downwards, for 4m <= x <= 10m
     ])
 
     # Create a new figure and axes
-    ax = plt.subplots()
-
-    # Use the plot method on the Beam object (without passing axes)
+    plt.subplots()
+    # Use the plot method on the Beam object
     beam.plot()
-
     # Save the plot to a BytesIO object
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
